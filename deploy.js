0347b91c75daa54ac41b69fe15fe50a7e3b2129a
FrankiePower <franklin.power21@gmail.com>
2024-05-29 17:35:21 +0100

    Inbox Smart Contract

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  // Account mmenomic phrase!
+  "seat bus glance unfold lamp short van couch cake vanish into spell",
+
+  // Infura endpoint!
+  "https://sepolia.infura.io/v3/2c7f6d8116fd45adb63c13f00c776087"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
